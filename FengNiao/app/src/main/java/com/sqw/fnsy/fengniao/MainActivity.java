03a05ebed191003f58cf8ded1e8f6c914be2c48f
sunyingbo <1791410964@qq.com>
2016-09-30 13:34:46 +0800

    底部导航栏布局

--- FengNiao/app/src/main/java/com/sqw/fnsy/fengniao/MainActivity.java
+++ FengNiao/app/src/main/java/com/sqw/fnsy/fengniao/MainActivity.java
@@ -2,12 +2,20 @@ package com.sqw.fnsy.fengniao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        radioGroup = (RadioGroup) findViewById(R.id.rg_mainactivity);
     }
 }
